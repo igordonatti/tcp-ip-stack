5f8f7ed7abd2ab8e426ee625984e9a4ef60570e4
Igor Donatti <igordonatti.id@gmail.com>
2025-10-20 17:04:31 -0300

    first commit

---
+++ TCPIPSTACK.cpp
@@ -0,0 +1,12 @@
+﻿// TCPIPSTACK.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include "TCPIPSTACK.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
